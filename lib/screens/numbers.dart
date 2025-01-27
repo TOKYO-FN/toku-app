3f0c4b5f0af61aa45401dcb7a9eb70c5caf84a1c
TOKYO-FN <youssef.magdyii.12@gmail.com>
2025-01-28 00:00:27 +0200

    testing

--- lib/screens/numbers.dart
+++ lib/screens/numbers.dart
@@ -8,13 +8,60 @@ class Numbers extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xffFFF4D9),
       appBar: AppBar(
-        backgroundColor: Color(0xffef9235),
+        backgroundColor: Color(0xff46322B),
         title: Text(
           "Numbers",
           style: TextStyle(
             color: Colors.white,
             fontWeight: FontWeight.bold,
           ),
+        ),
+      ),
+      body: Container(
+        color: Color(0xffef9235),
+        height: 100,
+        child: Row(
+          children: [
+            Container(
+              color: Color(0xffFFF4D9),
+              child: Image.asset(
+                'assets/images/numbers/number_one.png',
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "ichi",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                  Text(
+                    "one",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Spacer(
+              flex: 1,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 16),
+              child: Icon(
+                Icons.play_arrow,
+                color: Colors.white,
+                size: 30,
+              ),
+            ),
+          ],
         ),
       ),
     );
